f6b9e65eb88de72ada7e33a4950009c2496e2907
lunga <stumenz.complex@gmail.com>
2019-08-15 12:56:43 +0200

    better naming

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="wazimap-dymanic-profile",
+    name="dymanic-profile",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
-    description="Django app to create dymanic census data.",
+    description="Django app to create dymanic census data for wazimap.",
     long_description=README,
     url="https://github.com/OpenUpSA/wazimap-dynamic-profile",
     author="OpenUpSA",
