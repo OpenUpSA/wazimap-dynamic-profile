191a8112b6ac786638b01d81c2a347dabc643ce1
lunga <stumenz.complex@gmail.com>
2019-09-12 16:33:21 +0200

    model closer to wazimap_get_stat_data

--- dynamic_profile/models.py
+++ dynamic_profile/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from wazimap.models import FieldTable
+from django.contrib.postgres.fields import HStoreField, ArrayField
 
 # Create your models here.
 
@@ -18,22 +19,23 @@ class Profile(models.Model):
 class IndicatorProfile(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     table_name = models.ForeignKey(FieldTable, on_delete=models.CASCADE)
-    column_name = models.CharField(max_length=50, null=True)
-    header = models.CharField(max_length=30)
+    field_name = models.CharField(max_length=50, null=True)
+    title = models.CharField(max_length=30)
     summary = models.CharField(max_length=100)
     chart_title = models.CharField(max_length=100)
     chart_type = models.CharField(max_length=20, default="histogram")
-    order_by = models.BooleanField(default=False)
+    order_by = models.CharField(max_length=10, default="-total")
     exclude_zero = models.BooleanField(default=False)
-    percent = models.BooleanField(default=False)
-    maximum_value = models.CharField(max_length=20, default="distribution")
-    column_field = models.CharField(max_length=30, null=True, blank=True)
+    percent = models.BooleanField(default=True)
+    recode = HStoreField(null=True)
+    key_order = ArrayField(null=True)
+    exclude = ArrayField(null=True)
     display_order = models.IntegerField(default=1)
     parent_profile = models.ForeignKey(
         "self", on_delete=models.CASCADE, null=True, blank=True
     )
     group_remainder = models.IntegerField(null=True, blank=True)
-    disclaimer_text = models.TextField(null=True, blank=True)
+    info = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.header
